cc91f6a6cf9418921aae8686430677f1d5670297
Dmitry V. Zimin <pfzim@mail.ru>
2017-09-25 19:40:32 +0300

    Update script for purge folder from files which did not change more that 21 days

--- purge_exchanger.sh
+++ purge_exchanger.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-find . -not -path "./_21_day_automatic_clean*" -a -not -newerBt "`date --date='21 days ago'`" -a -mtime +21 -a -ctime +21 -print0
+find . -not -path "./_21_day_automatic_clean*" -a -type f -a -not -newerBt "21 days ago" -a -mtime +21 -a -ctime +21 -print0 | xargs -0 -I {} sh -c '
+    file="{}"
+    destination="/home/DZimin/_21_day_automatic_clean/_21_day_automatic_clean"
+    mkdir -p "$destination/${file%/*}"
+    mv "$file" "$destination/$file"'
