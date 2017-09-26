0e81ca17fbfac4a16108a18cfe65fd50681c17c6
Dmitry V. Zimin <pfzim@mail.ru>
2017-09-26 17:08:58 +0300

    Update script for automatic purge exchange folder

--- purge_exchanger.sh
+++ purge_exchanger.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 
-find . -not -path "./_21_day_automatic_clean*" -a -type f -a -not -newerBt "21 days ago" -a -mtime +21 -a -ctime +21 -print0 | xargs -0 -I {} sh -c '
-    file="{}"
-    destination="/home/DZimin/_21_day_automatic_clean/_21_day_automatic_clean"
-    mkdir -p "$destination/${file%/*}"
-    mv "$file" "$destination/$file"'
+# This script for clear shared folder in 2 steps
+# Step 1: Move files that does not created and not modified since last XX days to subfolder _XX_day_automatic_clean
+# Step 2: If moved files not modified in XX days it will be deleted
+
+days=21
+src=/share/public/exchange
+
+# move files to trash
+eval "find \"$src/\" -not -path \"$src/_${days}_day_automatic_clean/*\" -a -type f -a -not -newerBt \"$days days ago\" -a -mtime +$days -a -ctime +$days -print0 | sed -z -e \"s|^$src/||\" | xargs -0 -I {} sh -c 'file=\"{}\"; mkdir -p \"$src/_${days}_day_automatic_clean/\${file%/*}\"; mv \"$src/\$file\" \"$src/_${days}_day_automatic_clean/\$file\"'; touch \"$src/_${days}_day_automatic_clean/$file\""
+
+# purge trash
+find "$src/_${days}_day_automatic_clean" -type f -a -not -newerBt "$days days ago" -a -mtime +$days -a -ctime +$days -delete
