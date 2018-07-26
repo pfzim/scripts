95d9debaa0b343c51f69e7b725d25099c055c664
Dmitry V. Zimin <pfzim@mail.ru>
2018-07-26 16:07:02 +0300

    Add script for remove PC from AD, DNS, SCCM

---
+++ orchestrator/Remove-PC-From-AD-DNS-SCCM.ps1
@@ -0,0 +1,106 @@
+$global:compname = ""
+
+$global:result = 1
+$global:error_msg = ""
+
+function main()
+{
+	if($global:compname -notmatch '^\d\d-\d\d\d\d-\d+$')
+	{
+		$global:error_msg = "Неправильное имя ПК. Формат: NN-NNNN-N"
+		return
+	}
+
+	$comp = 0
+	try
+	{
+		$comp = Get-ADComputer -Identity $global:compname
+	}
+	catch
+	{
+		$global:error_msg += "ПК не найден в AD;`r`n"
+	}
+
+	try
+	{
+		if($comp)
+		{
+			Remove-ADComputer -Identity $comp -Confirm:$false
+		}
+	}
+	catch
+	{
+		$global:result = 2
+		$global:error_msg += "Ошибка удаления записи из AD;`r`n"
+	}
+
+	$dns = 0
+
+	try
+	{
+		$dns = Get-DnsServerResourceRecord -ComputerName brc-dc-01 -ZoneName "contoso.com" -Name $global:compname -ErrorAction SilentlyContinue
+	}
+	catch
+	{
+		$global:error_msg += "Запись в DNS не найдена;`r`n"
+	}
+
+	try
+	{
+		if($dns)
+		{
+			$dns | Remove-DnsServerResourceRecord -ComputerName brc-dc-01 -ZoneName "contoso.com" -Force
+		}
+		else
+		{
+			$global:error_msg += "Запись в DNS не найдена;`r`n"
+		}
+	}
+	catch
+	{
+		$global:result = 2
+		$global:error_msg += "Ошибка удаления DNS записи;`r`n"
+	}
+
+	$sccmServer='brc-sccm-01.contoso.com'
+	$sccmSite='M01'
+
+	$comp = 0
+
+	try
+	{
+		$comp = get-wmiobject -query "select * from SMS_R_SYSTEM WHERE Name='$global:compname'" -computername $sccmServer -namespace "ROOT\SMS\site_$sccmSite"
+	}
+	catch
+	{
+		$global:error_msg += "Запись в SCCM не найдена;`r`n"
+	}
+
+	try
+	{
+		if($comp)
+		{
+			$comp.psbase.delete()
+		}
+		else
+		{
+			$global:error_msg += "Запись в SCCM не найдена;`r`n"
+		}
+	}
+	catch
+	{
+		$global:result = 2
+		$global:error_msg += "Ошибка удаления записи из SCCM;`r`n"
+	}
+
+	if($global:result -ne 2)
+	{
+		$global:result = 0
+	}
+	else
+	{
+		$global:result = 1
+	}
+}
+
+main
